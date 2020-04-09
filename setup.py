aa10f4f785b5bcc07d4c0b77860f08131ca8ad0e
Joshua Powell <joshuapowell@users.noreply.github.com>
2020-04-09 17:09:19 -0400

    Added default gitignore, MANIFEST, README, and setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='dismount',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe = False,
+    install_requires=[
+        'flask',
+    ],
+)
